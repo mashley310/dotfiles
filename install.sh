d9b7d4e2e06d8c5c535f04c8915a02234cb139b5
Meghan Kelley <m_ashley_95@yahoo.com>
2025-12-31 14:24:12 -0500

    pull over all the powerlevel10k files

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 
-cp ./powerlevel10k/powerlevel10k.zsh-theme ~
 cp ./.p10k.zsh ~
 cp ./.zshrc ~
